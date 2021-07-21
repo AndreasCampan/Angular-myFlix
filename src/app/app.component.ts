9f5cb7e170898a28693361b41110666afd4308b3
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-07-21 11:39:31 -0600

    Updated based on task 6.3
    -added registration form and login form
    -installed angular material and cdk
    -created registration and login components\
    -modified some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myflix';
+  title = 'myFlix-Angular-client';
+
+  constructor(public dialog: MatDialog) { }
+
+  // Directs to registration modal
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+  // Assigning the dialog a width
+    width: '280px'
+    });
+  }
+
+  // Directs to login modal
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {});
+  }
 }
---
+++ src/app/user-login-form/user-login-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { UserLoginService } from '../fetch-api-data.service';
+
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss']
+})
+export class UserLoginFormComponent implements OnInit {
+
+  @Input() userData ={ Username: '', Password: ''};
+
+  constructor(
+    public fetchApiData: UserLoginService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void { }
+
+  /**
+   * function to login user
+   */
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      this.dialogRef.close();
+      localStorage.setItem('user', response.user.Username);
+      localStorage.setItem('token', response.token);
+      this.snackBar.open('User logged in successfully!', 'OK', {
+        duration: 2000
+      });
+      this.router.navigate(['movies']);
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
